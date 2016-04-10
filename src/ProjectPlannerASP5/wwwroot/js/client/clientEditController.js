8f06ebd9dc4f7b03d72675ea5d8f55a05637b086
Pawel <pawel.p.maga@gmail.com>
2016-04-10 11:28:46 +0200

    client list

--- src/ProjectPlannerASP5/wwwroot/js/client/clientEditController.js
+++ src/ProjectPlannerASP5/wwwroot/js/client/clientEditController.js
@@ -8,6 +8,35 @@
 
     function clientEditController(Client, $scope, $routeParams, $location)
     {
-            
+        if ($routeParams.id > 0 && $routeParams.projectCode) {
+            $scope.client = Client.get({ id: $routeParams.id });
+        } else {
+            $scope.client = new Client();
+            $scope.client.id = 0;
+            $scope.client.createTimeStamp = new Date();
+            $scope.client.lastUpdateTimeStamp = new Date();
+        }
+
+        $scope.projectCode = $routeParams.projectCode;
+
+        $scope.saveIssue = function () {
+            if ($scope.client.id === 0) {
+                $scope.client.$save().then(function () {
+                    $scope.goToClientList();
+                });
+            } else {
+                $scope.client.$update().then(function () {
+                    $scope.goToClientList();
+                });
+            }
+        };
+
+        $scope.cancelEdit = function () {
+            $scope.goToClientList();
+        };
+
+        $scope.goToClientList = function () {
+            $location.path('/Clients');
+        };
     };
 })();
--- src/ProjectPlannerASP5/wwwroot/js/client/clientsListController.js
+++ src/ProjectPlannerASP5/wwwroot/js/client/clientsListController.js
@@ -9,6 +9,35 @@
 
 
     function clientsListController(Client, $scope) {
-        
+
+        $scope.clients = [];
+        $scope.filteredClients = [];
+        $scope.errorMessage = "";
+        $scope.isBusy = true;
+        $scope.searchText = "";
+
+        Client.query().$promise
+            .then(function (response) {
+                angular.copy(response, $scope.clients);
+            },
+            function (error) {
+                $scope.errorMessage = "Failed to load data: " + error;
+            })
+            .finally(function () {
+                $scope.isBusy = false;
+            });
+
+        $scope.deleteClient = function (client) {
+            project.$delete(function () {
+                var index = $scope.issues.indexOf(issue);
+                $scope.projects.splice(index, 1);
+            });
+        };
+
+        $scope.getStatusClass = function (status) {
+            if (status === "Added")
+                return "label label-primary";
+            return "label";
+        };
     };
 })();
